545396116103d631a168656ee03bcaab9e36ccf5
quantumduck <quantum.duck@hotmail.com>
2017-04-29 13:17:52 -0400

    added more add character functionality and removed old code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var drawingArea = {
-  contents: [],
+  lines: [],
   width: 0,
   height: 0
 };
@@ -50,7 +50,7 @@ function drawingAreaInit(x, y) {
     for (var j = 0; i < y; j++) {
       line += ' ';
     }
-    drawingArea.contents.push(line);
+    drawingArea.lines.push(line);
   }
   drawingArea.width = x;
   drawingArea.height = y;
@@ -82,7 +82,7 @@ function redraw() {
   var html = "";
   for (y = 0; y < drawingArea.height; y++) {
     for (x = 0; x < drawingArea.width; x++) {
-      var char = drawingArea.contents[y][x];
+      var char = drawingArea.lines[y][x];
       switch (char) {
         case '&':
           html += '&amp;';
@@ -115,77 +115,66 @@ function getX(rawX) {
     numFromPixels($('#drawing-container').css('padding-left')) +
     numFromPixels($('#drawing-container').css('border-left'))
   );
-  var x = Math.floor(gameBoard[0].length * (rawX - offset_left) / width);
-  // if ((x % xSkip) === (xSkip - 1)) {
-  //   return -1;
-  // }
-  // console.log(Math.floor(x  / xSkip));
-  // return Math.floor(x / xSkip);
+  var x = Math.floor(drawingArea.width * (rawX - offset_left) / width);
   return x;
 }
 
 function getY(rawY) {
-  var height =  numFromPixels($('#game').css('height'));
-  var offset_top = sumNumFromPixels([
-    $('body').css('margin-top'),
-    $('h1').css('height'),
-    $('h1').css('margin-top'),
-    $('h1').css('margin-bottom'),
-  ]);
-  var y = Math.floor(gameBoard.length * (rawY - offset_top) / height);
-  console.log(y);
-  if ((y % ySkip) === (ySkip - 1)) {
-    return -1;
-  }
-  console.log(Math.floor(y  / ySkip));
-  return Math.floor(y  / ySkip);
+  var height =  numFromPixels($('#drawingArea').css('height'));
+  var offset_top = (
+    numFromPixels($('body').css('margin-top')) +
+    numFromPixels($('h1').css('margin-top')) +
+    numFromPixels($('h1').css('height')) +
+    Math.max(
+      numFromPixels($('hl').css('margin-bottom')),
+      numFromPixels($('.messages').css('margin-top'))
+    ) +
+    numFromPixels($('.messages').css('height')) +
+    numFromPixels($('.messages').css('margin-bottom')) +
+    numFromPixels($('#drawing-container').css('padding-top')) +
+    numFromPixels($('#drawing-container').css('border-top'))
+  );
+  var y = Math.floor(drawingArea.height * (rawY - offset_top) / height);
+  return y;
 }
 
 function numFromPixels(string) {
-  return Number(string.substring(0,string.indexOf('px')));
-}
-
-function sumNumFromPixels(strings) {
-  var sum = 0;
-  for (i = 0; i < strings.length; i++) {
-    sum += numFromPixels(strings[i]);
-  }
-  return sum;
+  // Assuming string in the form '0px'
+  return Number(string.substring(0, string.indexOf('px')));
 }
 
 function addChar(char, x, y) {
-  // console.log(char + x + y);
-  if (charAt(x, y) != ' ') {
-    return charAt(x, y);
+  var = drawingArea.lines[y];
+  if (
+    // Do not insert control characters.
+    (char.length != 1) ||
+    (char.codePoint(0) < 32) ||
+    ((char.codePoint(0) >= 127) && (char.codePoint(0) <= 255))
+  ) {
+    return false;
   }
-  x = xStart + (x * xSkip);
-  y = yStart + (y * ySkip);
-  var line = gameBoard[y];
   line = line.substring(0, x) + char + line.substring(x + 1, line.length);
-  gameBoard[y] = line;
-  drawBoard();
+  drawingArea.lines[y] = line;
+  redraw();
   return char;
 }
 
 function charAt(x, y) {
-  return gameBoard[yStart + (y * ySkip)][xStart + (x * xSkip)];
+  return drawingArea.lines[y][x];
 }
 
-function aiHard() {
-  if (winInOne()) {
-    // If there is a spot where either user can win, play there.
-    addChar(aiChar, winInOne().x, winInOne().y);
-  } else if (charAt(1, 1) === ' ') {
-    // If the center is untaken, take it.
-    addChar(aiChar, 1, 1);
+function addRandomChar() {
+  var slots = emptySquares();
+  var slot;
+  var char;
+  if (slots.length) {
+    slot = slots(Math.floor(Math.random() * slots.length));
+    char = String.fromCodePoint(32 + Math.floor(Math.random() * 95));
+    return addChar(char, slot.x, slot.y);
   } else {
-    // Play in a corner, if possible.
+    return false;
   }
-}
 
-function ai() {
-  var x = 1;
-  var y = 1;
   while (charAt(x, y) != ' ') {
     var x = Math.floor(Math.random() * 3);
     var y = Math.floor(Math.random() * 3);
@@ -193,53 +182,6 @@ function ai() {
   addChar(aiChar, x, y);
 }
 
-function winCondition() {
-  for (var l = 0; l < lines.length; l++) {
-    var line = lines[l];
-    // console.log(line);
-    var char = charAt(line[0].x, line[0].y);
-    if (char != ' ') {
-      var i = 0;
-      while (char === charAt(line[i].x, line[i].y)) {
-        if (++i === gridSize) {
-          break;
-        }
-      }
-      if (i === gridSize) {
-        return line;
-      }
-    }
-  }
-  return false;
-}
-
-function winInOne() {
-  var results = [];
-  for (var l = 0; l < lines.length; l++) {
-    var line = lines[l];
-    // console.log(line);
-    for (var i = 0; i < gridSize; i++) {
-      if (charAt(line[i].x, line[i].y) === ' ') {
-        var k = anyOtherIndex(i, gridSize);
-        var char = charAt(line[k].x, line[k].y);
-        if (char != ' ') {
-          for (var j = 0; j < gridSize; j++) {
-            if ((j != i) && (charAt(line[j].x, line[j].y) != char)) {
-              break;
-            } else if (j === gridSize - 1) {
-              results.push([line[i], char]);
-            }
-          }
-        }
-      }
-    }
-  }
-  if (results.length === 0) {
-    return false;
-  } else {
-    return results;
-  }
-}
 
 $(function() {
     $('body').css('max-width', $('.drawing-container').css('width'));
