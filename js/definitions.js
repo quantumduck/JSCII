5e976a0c82025c4a4e79178c8086a3c55709a7b9
quantumduck <quantum.duck@hotmail.com>
2017-04-30 14:14:32 -0400

    adding methods to allow nesting of drawing objects

--- js/definitions.js
+++ js/definitions.js
@@ -14,8 +14,7 @@ var mode = {
 
 var currentMode = mode.overwrite;
 
-// This is where the ASCII data is stored:
-// There are two helper functions for getting the width and height.
+// DEPRECIATED SYNTAX! To be repaced by a class.
 var drawingArea = {
   lines: [""],
   width: function() {
@@ -26,10 +25,15 @@ var drawingArea = {
   }
 };
 
+// This is where the ASCII data is stored:
+// There are two helper functions for getting the width and height.
 class DrawingArea {
+
   constructor(width, height, content) {
     this.children = [];
     this.lines = [];
+    this.parent = false;
+    this.offset = {left: 0; top: 0};
     for (var y = 0; y < height; y++) {
       this.lines.push("");
       for (var x = 0; x < width; x++) {
@@ -41,6 +45,7 @@ class DrawingArea {
       }
     }
   }
+
   get height() {
     return this.lines.length;
   }
@@ -48,6 +53,63 @@ class DrawingArea {
   get width() {
     return this.lines[0].length;
   }
+
+  addChild(offsetLeft, offsetTop, input) {
+    var child = input;
+    child.parent = this;
+    child.offset = {left: offsetLeft, top: offsetTop};
+    this.children.push(child);
+    return child;
+  }
+
+  indexOf(child) {
+    var index = -1;
+    for (var i = 0; i < this.children.length; i++) {
+      if (child == this.children[i]) {
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  removeChild(input) {
+    var index = this.indexOf(input);
+    if (index < 0) {
+      return false;
+    } else {
+      var child = this.children[index];
+      child.parent = false;
+      child.offset = {left: 0, top: 0};
+      for (var i = index; i < this.children.length - 1; i++) {
+        this.children[i] = this.children[i + 1];
+      }
+      this.children.pop();
+      return child;
+    }
+  }
+
+  move(x, y) {
+    if (this.parent) {
+      return this.parent.moveChild(this, x, y);
+    } else {
+      return false;
+    }
+  }
+
+  moveChild(input, x, y) {
+    var index = this.indexOf(input);
+    if (index < 0) {
+      return false;
+    } else {
+      var child = this.children[index];
+      child.offset.left += x;
+      child.offset.top += y;
+      this.children[index] = child;
+      return child;
+    }
+  }
+
+
 }
 
 var da = new DrawingArea(3, 3);
