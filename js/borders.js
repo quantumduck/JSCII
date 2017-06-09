9f07ce7342d9aad26cdebfaac971d0f3cfab194b
quantumduck <quantum.duck@hotmail.com>
2017-06-09 18:17:24 -0400

    borders now drawn externally

--- js/borders.js
+++ js/borders.js
@@ -79,81 +79,108 @@ function borderPattern(area, edge) {
   return false;
 }
 
-function setBorder(area, edge, pattern) {
-  var output = area;
+function setBorders(area, topPattern, leftPattern, bottomPattern, rightPattern) {
+  var output = interior(area);
+  var selection = selectAll(output);
+  var borders = {};
   var borderData = {};
-  if (pattern) {
-    var selection = selectAll(area);
-    switch (edge) {
-      case 'top': {
-        selection.ymax = selection.ymin + pattern.length - 1;
-        borderData.height = pattern.length;
-        borderData.rep = pattern[0].length;
-        break;
-      }
-      case 'bottom': {
-        selection.ymin = selection.ymax - pattern.length;
-        borderData.height = pattern.length;
-        borderData.rep = pattern[0].length;
-        break;
-      }
-      case 'left': {
-        selection.xmax = selection.xmin + pattern[0].length - 1;
-        borderData.width = pattern[0].length;
-        borderData.rep = pattern.length;
-        break;
-      }
-      case 'right': {
-        selection.xmin = selection.xmax - pattern[0].length;
-        borderData.width = pattern[0].length;
-        borderData.rep = pattern.length;
-        break;
-      }
-    }
-    var border = subArea(area, selection);
-    border.lines = contentFill(border.width, border.height, pattern);
-    border.opaque = true;
-    output.lines = mergeAreas(border, area).lines;
-    output.border[edge] = borderData;
-    return output;
-  } else {
-    switch (edge) {
-      case 'top':
-      case 'bottom': {
-        borderData = {height: 0, rep: 1};
-        break;
-      }
-      case 'left':
-      case 'right': {
-        borderData = {width: 0, rep: 1};
-        break;
-      }
-    }
-    output.border[edge] = borderData;
-    return output;
+  if (!leftPattern) {
+    leftPattern = topPattern;
   }
+  if (!rightPattern) {
+    rightPattern = leftPattern;
+  }
+  if (!bottomPattern) {
+    bottomPattern = topPattern;
+  }
+  if (topPattern.length > 0) {
+    selection.ymax = selection.ymin - 1;
+    selection.ymin -= topPattern.length;
+    if (leftPattern[0] && leftPattern[0].length > 0) {
+      selection.xmin -= leftPattern[0].length;
+    }
+    if (rightPattern[0] && rightPattern[0].length > 0) {
+      selection.xmax += rightPattern[0].length;
+    }
+    borders.top = areaInit(selection);
+    borders.top.lines = contentFill(
+      borders.top.width,
+      borders.top.height,
+      topPattern
+    );
+    output = mergeAreas(output, borders.top);
+    borderData.top = {height: topPattern.length, rep: topPattern[0].length};
+  } else {
+    borderData.top = {height: 0, rep: 1};
+  }
+  if (bottomPattern.length > 0) {
+    selection.ymin = selection.ymax + 1;
+    selection.ymax += bottomPattern.length;
+    if (leftPattern[0] && leftPattern[0].length > 0) {
+      selection.xmin -= leftPattern[0].length;
+    }
+    if (rightPattern[0] && rightPattern[0].length > 0) {
+      selection.xmax += rightPattern[0].length;
+    }
+    borders.bottom = areaInit(selection);
+    borders.bottom.lines = contentFill(
+      borders.bottom.width,
+      borders.bottom.height,
+      topPattern
+    );
+    output = mergeAreas(output, borders.bottom);
+    borderData.bottom = {height: bottomPattern.length, rep: bottomPattern[0].length};
+  } else {
+    borderData.bottom = {height: 0, rep: 1};
+  }
+  if (leftPattern.length > 0) {
+    selection.xmax = selection.xmin - 1;
+    selection.xmin -= leftPattern.length;
+    borders.left = areaInit(selection);
+    borders.left.lines = contentFill(
+      borders.left.width,
+      borders.left.height,
+      leftPattern
+    );
+    output = mergeAreas(output, borders.left);
+    borderData.left = {height: leftPattern.length, rep: leftPattern[0].length};
+  } else {
+    borderData.left = {height: 0, rep: 1};
+  }
+  if (rightPattern.length > 0) {
+    selection.xmin = selection.xmax + 1;
+    selection.xmax += rightPattern.length;
+    borders.right = areaInit(selection);
+    borders.right.lines = contentFill(
+      borders.right.width,
+      borders.right.height,
+      rightPattern
+    );
+    output = mergeAreas(output, borders.right);
+    borderData.right = {height: bottomPattern.right, rep: rightPattern[0].length};
+  } else {
+    borderData.right = {height: 0, rep: 1};
+  }
+  output.border = borderData;
+  return output;
 }
 
 function borderResize(area, selection) {
-  var output = toRectangle(subArea(area.interior(), selection));
+  var output = subArea(area.interior(), selection);
   var corners = [
     {v: 'top', h:'left', cor: corner(area, 'top', 'left')},
     {v: 'top', h:'right', cor: corner(area, 'top', 'right')},
     {v: 'bottom', h:'right', cor: corner(area, 'bottom', 'right')},
     {v: 'bottom', h:'left', cor: corner(area, 'bottom', 'left')}
   ];
-  var edges = [
-    {edge: 'top', pat: borderPattern(area, 'top')},
-    {edge: 'left', pat: borderPattern(area, 'left')},
-    {edge: 'right', pat: borderPattern(area, 'right')},
-    {edge: 'bottom', pat: borderPattern(area, 'bottom')}
-  ];
+  var edges = {
+    top: borderPattern(area, 'top'),
+    left: borderPattern(area, 'left'),
+    right: borderPattern(area, 'right'),
+    bottom: borderPattern(area, 'bottom')
+  };
   // Note: settng border expands the area.
-  for (var i = 0; i < edges.length; i++) {
-    if (edges[i].pat) {
-      output = setBorder(output, edges[i].edge, edges[i].pat);
-    }
-  }
+  output = setBorders(output, edges.top, edges.left, edges.bottom, edges.right);
   for (var i = 0; i < corners.length; i++) {
     if (corners[i].cor) {
       output = setCorner(output, corners[i].v, corners[i].h, corners[i].cor.lines);
