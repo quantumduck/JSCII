4264f2c86a51471991b40ee293eb1371e9dec62e
quantumduck <quantum.duck@hotmail.com>
2021-03-06 09:12:13 -0500

    adding dev folder for building

---
+++ dev/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testRegex: '.(test|spec).(j|t)s$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/run.ts'],
+    testEnvironment: 'node',
+    resetMocks: true,
+    coverageReporters: ['json-summary', 'text'],
+};
